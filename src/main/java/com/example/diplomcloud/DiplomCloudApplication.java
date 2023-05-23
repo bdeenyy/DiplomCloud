1639d9baacd80ae92f6d120038aef6eda94739c6
denisbykov <i@deenyy.ru>
2023-05-23 15:52:49 +0300

    Initial commit

---
+++ src/main/java/com/example/diplomcloud/DiplomCloudApplication.java
@@ -0,0 +1,13 @@
+package com.example.diplomcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiplomCloudApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiplomCloudApplication.class, args);
+    }
+
+}
